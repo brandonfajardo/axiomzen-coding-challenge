bcde7fd424150432af618886dd2ee13a9dda0de6
Brandon Fajardo <brandon@applydigital.co>
2018-05-19 14:49:01 -0700

    Add stateful urls

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -1,47 +1,59 @@
 import {
-    FIND_REPO, 
-    FIND_REPO_SUCCESS, 
-    FIND_REPO_FAILURE,
-    CHANGE_ACTIVE_TAB,
-    CLOSE_REPO
-} from './types'
-import history from '../history'
-import axios from 'axios'
-import { getLastPageNumber } from '../util'
+  FIND_REPO,
+  FIND_REPO_SUCCESS,
+  FIND_REPO_FAILURE,
+  CHANGE_ACTIVE_TAB,
+  CLOSE_REPO
+} from "./types";
+import history from "../history";
+import axios from "axios";
+import { getLastPageNumber } from "../util";
 
 export const findRepo = (user, repoName, currentPageNumber) => dispatch => {
-    
-    dispatch({ type: FIND_REPO })
-    
-    axios.get(`https://api.github.com/repos/${user}/${repoName}/issues?page=${currentPageNumber}&state=all`)
-        .then(({ data, headers }) => {
-            const lastPageNumber = getLastPageNumber(headers)
+  dispatch({ type: FIND_REPO });
 
-            return {
-                lastPageNumber,
-                data
-            }
-        })
-        .then(repo => {
-            const { data, lastPageNumber } = repo
-            if (data.length){
-                setTimeout(() => { // Responding too quickly so I added 1s of delay
-                    dispatch(({ 
-                        type: FIND_REPO_SUCCESS,
-                        payload: { data, user, repoName, currentPageNumber, lastPageNumber }
-                    }))
-                    history.push('/issues')
-                }, 1000)
-            }
-        })
-        .catch((e) => {
-            dispatch({ type: FIND_REPO_FAILURE, payload: 'The following repo does not have any issues or there was an error in your request.' })
-        })
-}
+  axios
+    .get(
+      `https://api.github.com/repos/${user}/${repoName}/issues?page=${currentPageNumber}&state=all`
+    )
+    .then(({ data, headers }) => {
+      const lastPageNumber = getLastPageNumber(headers);
+
+      return {
+        lastPageNumber,
+        data
+      };
+    })
+    .then(repo => {
+      const { data, lastPageNumber } = repo;
+      if (data.length) {
+        setTimeout(() => {
+          // Responding too quickly so I added 1s of delay
+          dispatch({
+            type: FIND_REPO_SUCCESS,
+            payload: { data, user, repoName, currentPageNumber, lastPageNumber }
+          });
+          history.push(
+            `/issues?user=${user}&repoName=${repoName}&page=${currentPageNumber}`
+          );
+        }, 1000);
+      }
+    })
+    .catch(e => {
+      dispatch({
+        type: FIND_REPO_FAILURE,
+        payload:
+          "The following repo does not have any issues or there was an error in your request."
+      });
+    });
+};
 
 export const closeRepo = () => dispatch => {
-    dispatch({ type: CLOSE_REPO })
-    history.push('/')
-}
+  dispatch({ type: CLOSE_REPO });
+  history.push("/");
+};
 
-export const changeActiveTab = tab => ({ type: CHANGE_ACTIVE_TAB, payload: tab })
+export const changeActiveTab = tab => ({
+  type: CHANGE_ACTIVE_TAB,
+  payload: tab
+});
--- app/src/pages/IssuesList/index.js
+++ app/src/pages/IssuesList/index.js
@@ -1,91 +1,143 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import Issue from './Issue'
-import { Container, Row, Col } from 'react-grid-system'
-import _ from 'lodash'
-import { 
-    findRepo,
-    closeRepo,
-    changeActiveTab,
-} from '../../actions'
-import { Header } from '../../components'
-import closeIcon from '../../styles/icons/close.svg'
-import './index.scss'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Issue from "./Issue";
+import { Container, Row, Col } from "react-grid-system";
+import _ from "lodash";
+import { findRepo, closeRepo, changeActiveTab } from "../../actions";
+import { Header } from "../../components";
+import closeIcon from "../../styles/icons/close.svg";
+import "./index.scss";
+import qs from "querystringify";
 
 class IssuesList extends Component {
-    componentWillMount() {
-        window.addEventListener('scroll', this.onScroll, false);
+  componentWillMount() {
+    window.addEventListener("scroll", this.onScroll, false);
+
+    const { user, repoName, page } = qs.parse(this.props.location.search);
+
+    if (user && repoName && page) {
+      this.props.findRepo(user, repoName, Number(page));
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll, false);
+  }
+
+  onScroll = () => {
+    const {
+      findRepo,
+      currentPageNumber,
+      lastPageNumber,
+      repoName,
+      user,
+      allIssues,
+      loading,
+      error
+    } = this.props;
+    if (
+      window.innerHeight + window.scrollY >= document.body.offsetHeight - 50 &&
+      allIssues.length &&
+      !loading &&
+      !error &&
+      currentPageNumber <= lastPageNumber
+    ) {
+      findRepo(user, repoName, currentPageNumber + 1);
+    }
+  };
+
+  render() {
+    const {
+      allIssues,
+      loading,
+      tabActive,
+      changeActiveTab,
+      openIssues,
+      closedIssues,
+      prIssues,
+      closeRepo,
+      repoName,
+      user
+    } = this.props;
+
+    let listToRender;
+    if (tabActive === "openIssues") {
+      listToRender = openIssues;
+    } else if (tabActive === "closedIssues") {
+      listToRender = closedIssues;
+    } else if (tabActive === "prIssues") {
+      listToRender = prIssues;
+    } else if (tabActive === "allIssues") {
+      listToRender = allIssues;
     }
 
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.onScroll, false);
-    }
-
-    onScroll = () => {
-        const { findRepo, currentPageNumber, lastPageNumber, repoName, user, allIssues, loading, error } = this.props
-        if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 50) && allIssues.length && !loading && !error && currentPageNumber <= lastPageNumber) {
-            findRepo(user, repoName, currentPageNumber + 1)
-        }
-    }
-    
-    render() {
-        const { 
-            allIssues, 
-            loading, 
-            tabActive, 
-            changeActiveTab, 
-            openIssues, 
-            closedIssues, 
-            prIssues, 
-            closeRepo, 
-            repoName, 
-            user 
-        } = this.props
-
-        let listToRender
-        if (tabActive === 'openIssues'){
-            listToRender = openIssues
-        } else if (tabActive === 'closedIssues'){
-            listToRender = closedIssues
-        } else if (tabActive === 'prIssues'){
-            listToRender = prIssues
-        } else if (tabActive === 'allIssues'){
-            listToRender = allIssues
-        }
-
-        const issues = _.chunk(listToRender, 3)
-        return (
-            <div>
-                <Header user={user} repoName={repoName} />
-                <Container>
-                    <div className="tabContainer">
-                        <div className="tabContainerSpace">
-                            <div className="tabContainerRow">
-                                <p onClick={() => changeActiveTab('allIssues')} className={tabActive === 'allIssues' ? 'tabActive' : 'tabInactive'}>All Issues</p>
-                                <p onClick={() => changeActiveTab('openIssues')} className={tabActive === 'openIssues' ? 'tabActive' : 'tabInactive'}>Open Issues</p>
-                                <p onClick={() => changeActiveTab('closedIssues')} className={tabActive === 'closedIssues' ? 'tabActive' : 'tabInactive'}>Closed Issues</p>
-                                <p onClick={() => changeActiveTab('prIssues')} className={tabActive === 'prIssues' ? 'tabActive' : 'tabInactive'}>Pull Requests</p>
-                            </div>
-                            <img alt="" src={closeIcon} onClick={() => closeRepo()} className="close" />
-                        </div>
-                    </div>
-                    
-                    {issues && issues.map((issueArr, i) => (
-                        <Row key={`issueArray-${i}`}>
-                            {issueArr.map((issue, i) => (
-                                <Col key={`issue-${i}`} lg={4}>
-                                    <Issue {...issue} />
-                                </Col>
-                            ))}
-                        </Row>
-                    ))}
-                    {loading && <p className="loading">Loading...</p>}
-                </Container>
+    const issues = _.chunk(listToRender, 3);
+    return (
+      <div>
+        <Header user={user} repoName={repoName} />
+        <Container>
+          <div className="tabContainer">
+            <div className="tabContainerSpace">
+              <div className="tabContainerRow">
+                <p
+                  onClick={() => changeActiveTab("allIssues")}
+                  className={
+                    tabActive === "allIssues" ? "tabActive" : "tabInactive"
+                  }
+                >
+                  All Issues
+                </p>
+                <p
+                  onClick={() => changeActiveTab("openIssues")}
+                  className={
+                    tabActive === "openIssues" ? "tabActive" : "tabInactive"
+                  }
+                >
+                  Open Issues
+                </p>
+                <p
+                  onClick={() => changeActiveTab("closedIssues")}
+                  className={
+                    tabActive === "closedIssues" ? "tabActive" : "tabInactive"
+                  }
+                >
+                  Closed Issues
+                </p>
+                <p
+                  onClick={() => changeActiveTab("prIssues")}
+                  className={
+                    tabActive === "prIssues" ? "tabActive" : "tabInactive"
+                  }
+                >
+                  Pull Requests
+                </p>
+              </div>
+              <img
+                alt=""
+                src={closeIcon}
+                onClick={() => closeRepo()}
+                className="close"
+              />
             </div>
-        )
-    }
+          </div>
+
+          {issues &&
+            issues.map((issueArr, i) => (
+              <Row key={`issueArray-${i}`}>
+                {issueArr.map((issue, i) => (
+                  <Col key={`issue-${i}`} lg={4}>
+                    <Issue {...issue} />
+                  </Col>
+                ))}
+              </Row>
+            ))}
+          {loading && <p className="loading">Loading...</p>}
+        </Container>
+      </div>
+    );
+  }
 }
 
-const mapState = ({ issue: { ...issue  }}) => ({ ...issue })
-const mapDispatch = { findRepo, changeActiveTab, closeRepo }
-export default connect(mapState, mapDispatch)(IssuesList)
+const mapState = ({ issue: { ...issue } }) => ({ ...issue });
+const mapDispatch = { findRepo, changeActiveTab, closeRepo };
+export default connect(mapState, mapDispatch)(IssuesList);
